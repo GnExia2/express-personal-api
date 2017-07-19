94c6b835688d98be43132e0e46b91c0947c342cd
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-19 12:13:31 -0700

    added GET path for campsites

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/api', function apiIndex(req, res) {
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/campsites", description: "E.g. shows new campsite"} // CHANGE ME
     ]
   })
 });
