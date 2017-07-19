ffc94fb8b76fd7aeb0167edd2972c36b3d6029a1
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-19 11:10:50 -0700

    changed URL some documentation

--- server.js
+++ server.js
@@ -49,12 +49,21 @@ app.get('/api', function apiIndex(req, res) {
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/GnExia2/express-personal-api/blob/master/README.md", // CHANGE ME
+    baseUrl: "https://enigmatic-wave-47277.herokuapp.com", // CHANGE ME
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET",
+       path: "/api",
+       description: "Describes all available endpoints"
+    },
+      {method: "GET",
+       path: "/api/profile",
+        description: "Data about me"
+    }, // CHANGE ME
+      {method: "POST",
+       path: "/api/campsites",
+       description: "E.g. Create a new campsite"
+    } // CHANGE ME
     ]
   })
 });
